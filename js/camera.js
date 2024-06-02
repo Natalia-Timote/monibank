b2b6078a242cff4578d9b4eead5360cefca1a605
Natalia-Timote <nataliamirian.nt@gmail.com>
2024-06-02 05:07:41 -0300

    enviar formulário

--- js/camera.js
+++ js/camera.js
@@ -1,7 +1,19 @@
+// Iniciar vídeo
 const startCameraBtn = document.querySelector('[data-video-botao]');
 const cameraField = document.querySelector('[data-camera]');
 const video = document.querySelector('[data-video]');
 
+// Tirar foto
+const takePictureBtn = document.querySelector('[data-tirar-foto]');
+const canvas = document.querySelector('[data-video-canvas]');
+const message = document.querySelector('[data-mensagem]');
+
+let imageURL = '';
+
+// Enviar foto
+const sendPictureBtn = document.querySelector('[data-enviar]');
+
+// Iniciar vídeo
 startCameraBtn.addEventListener('click', async function () {
     const startVideo = await navigator.mediaDevices.getUserMedia({video: true, audio: false});
 
@@ -9,4 +21,26 @@ startCameraBtn.addEventListener('click', async function () {
     cameraField.style.display = 'block';
 
     video.srcObject = startVideo;
+})
+
+ // Tirar foto
+takePictureBtn.addEventListener('click', function() {
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    imageURL = canvas.toDataURL('image/jpeg');
+
+    cameraField.style.display = 'none';
+    message.style.display = 'block';
+})
+
+// Enviar foto
+sendPictureBtn.addEventListener('click', () => {
+    const receiveExistingData = localStorage.getItem('cadastro');
+    const convertReturn = JSON.parse(receiveExistingData);
+
+    convertReturn.image = imageURL;
+
+    localStorage.setItem('cadastro', JSON.stringify(convertReturn));
+
+    window.location.href = './abrir-conta-form-3.html';
 })
